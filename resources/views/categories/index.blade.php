dafaddc3170bad747dac294a1a389ed35655f0b9
mofasa4 <moh.f.saleh4@gmail.com>
2020-10-12 18:35:29 +0200

    adding the feature of deleting the categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -21,7 +21,7 @@
                 <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-primary btn-sm float-right">Edit</a>
                 </td>
                 <td>
-                <button class="btn btn-danger btn-sm" wire:click="delete({{$category->id}})">delete</button>
+                <button class="btn btn-danger btn-sm" onclick="handleDelete({{$category->id}})">Delete</button>
                 </td>
             </tr>
         @endforeach
@@ -30,5 +30,48 @@
     </div>
 </div>
 
+<!-- Modal -->
+<form action="" method="POST" id="deleteForm">
+@csrf
+@method('DELETE')
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center">
+        Are You Sure You Want to Delete This Category
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-danger">Yes Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+
+@endsection
+
+@section('script')
+
+<script>
+
+    
+    
+    function handleDelete(id){
+        $('#deleteModal').modal('show');
+
+    var form = document.getElementById('deleteForm');
+    form.action = '/categories/' + id;
+    console.log(form)
+    }
+
+</script>
 
 @endsection
